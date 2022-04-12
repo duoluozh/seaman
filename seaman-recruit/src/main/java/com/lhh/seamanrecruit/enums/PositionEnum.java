db1ae384d9d29b39fd2bcaa463a18033525c6ad8
zhh <duoluozh@163.com>
2022-04-12 16:05:26 +0800

    职位枚举类

--- seaman-recruit/src/main/java/com/lhh/seamanrecruit/enums/PositionEnum.java
+++ seaman-recruit/src/main/java/com/lhh/seamanrecruit/enums/PositionEnum.java
@@ -3,22 +3,35 @@ package com.lhh.seamanrecruit.enums;
 /**
  * @author zhh
  * @date 2022/4/11 11:22
- * @description
+ * @description 职位枚举
  */
 public enum PositionEnum {
 
-    BULK_CARGO_SHIP("散货船"),
-    PASSENGER_CARGO_SHIP("客货船"),
-    CONTAINER_SHIP("集装箱船"),
-    RO_RO_SHIP("滚装船"),
-    CARGO_BARGE_SHIP("载驳货船"),
-    ORDINARY_CARGO_SHIP("普通货船"),
-    TANKER_SHIP("油船"),
-    LIQUEFIED_GAS_SHIP("液化气体船"),
-    DUAL_PURPOSE_SHIP("兼用船"),
-    OTHER_TYPES("其他类型");
+    CAPTAIN("船长"),
+    CHIEF_MATE("大副"),
+    SECOND_MATE("二副"),
+    THIRD_MATE("三副"),
+    BOATSWAIN("水手长"),
+    ONE_WATER("一水"),
+    TOW_WATER("二水"),
+    DECK_CADET("甲板实习生"),
+    CHEF("大厨"),
+    BUTTONS("服务生"),
+    CHIEF_ENGINEER("轮机长"),
+    LARGE_PIPE_WHEEL("大管轮"),
+    SECOND_PIPE_WHEEL("二管轮"),
+    THREE_TUBE_WHEEL("三管轮"),
+    ELECTRICAL_ENGINEER("电机员"),
+    MACHINE_FOREMAN("机工长"),
+    MECHANIC("机工"),
+    SECOND_MACHINE("二机"),
+    ENGINE_CADET("机舱实习生");
 
 
+    /**
+     *
+     * @param name
+     */
     private PositionEnum(String name) {
         this.name = name;
     }
